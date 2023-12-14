2a0d520a3833087b49434731415cd5be03aa72bf
Koketso <bigshowbiggy73@gmail.com>
2023-12-14 11:42:28 +0200

    rock

---
+++ main.js
@@ -0,0 +1,18 @@
+function game() {
+    //play the game
+    //play five rounds
+    //console based
+
+
+}
+function  playRound() {
+    const playerSelection=
+}
+
+function playerChoice() {
+    //get input from player
+}
+
+function computerChoice() {
+    //get random input
+}
