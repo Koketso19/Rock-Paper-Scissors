c590871912e3384772af2e272b992b934e12087f
Koketso <bigshowbiggy73@gmail.com>
2023-12-17 15:07:02 +0200

    new

--- main.js
+++ main.js
@@ -16,9 +16,28 @@ function playRounds() {
  
 function playerChoice () {
     //get input from player
+    let input = prompt('Type Rock, Paper, or scissors');
+    while (input ==null) {
+        input = prompt('Type Rock, Paper, or scissors');
+    }
+    input = input.toLowerCase();
+    let check = validateInput(input)
+    while(check ==false){
+        prompt(
+            "Type Rock, Paper, or scissors. Spelling needs to be exact, but capitilization doesnt matter"
+        );
+    }
+
 }
 
 function computerChoice () {
     //get random input for computer
     return choices[Math.floor(Math.random()*choices.length)]
 }
+
+function validateInput(choice){
+    return choices.Includes(choice);
+    
+}
+
+game();
